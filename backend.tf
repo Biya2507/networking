3a1c868fb53a9f02e0452a515df5b7dd3ee6f966
Biya2507 <biyasvarghese325@gmail.com>
2025-06-22 00:38:33 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "networking-ca1-20065124"
     key            = "terraform/terraform.tfstate"
-    region         = "eu-north-1"
+    region         = "us-east-1"
     encrypt        = true
   }
 }
