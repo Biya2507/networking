85e54ae3b8a871491c7149a1431099ca654009af
Biya <biyasvarghese325@gmail.com>
2025-07-06 16:22:50 +0100

    Update

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "vardhanavprojectx"
-    key            = "ec2-docker/terraform.tfstate"
+    bucket         = "biya2507"
+    key            = "terraform/terraform.tfstate"
     region         = "eu-central-1"
     encrypt        = true
   }
